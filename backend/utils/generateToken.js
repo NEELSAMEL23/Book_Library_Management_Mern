fc922c49c36d211158c469a795b8b6573e8bd931
NEELSAMEL23 <neelsamel3@gmail.com>
2025-08-18 16:54:05 +0530

    JWT updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,10 +7,11 @@ export default function generateToken(res, userId) {
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production", // true in prod
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
+
 
     return token; // optional, in case you also want to return it
 }
